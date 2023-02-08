2dcf1800a88ee2a90b32112e33c6b2d918ed2f6b
consultorsandro <consultorsandro@hotmail.com>
2023-02-08 19:26:08 -0300

    Teste Números ùnicos - Incompleto

---
+++ numerosUnicos/main.cpp
@@ -0,0 +1,55 @@
+//ESTE EXERCÍCIO NÃO FOI CONCLUÍDO!!!!!!!!!!
+
+#include <iostream>
+
+
+
+void unique_numbers( int numbers[], unsigned int collection_size){
+   //The array that is going to store unique data  
+   int unique_data [20];
+
+
+   unsigned int unique_counter{};//Initialized to zero. Counts the elements we have in 
+                                //the unique_data array
+
+   for(unsigned int i{}; i < collection_size; ++i){
+
+    bool already_in{false};
+
+    //See if  the element is not already in our unique_data
+    for(unsigned int j{};j < unique_counter; ++j){
+        
+      //If we find that the element is already in our unique_data array,
+      //we say that the element at current iteration is a duplicate and
+      //break out of the loop
+      if(unique_data[j] == numbers[i]){
+        already_in = true;
+        break;
+      }
+    }
+
+    //If the element isn't already in, we put it in and increment the counter
+    //Please understand the use of the postfix operator here.
+    if(!already_in){
+      unique_data[unique_counter++] = numbers[i];
+    }
+    
+  }
+
+  std::cout << "The collection contains " << unique_counter << " unique numbers, they are : ";
+
+  for(unsigned int i{};i < unique_counter ; ++i){
+    std::cout << unique_data[i] << " ";
+  }    
+
+
+
+int main(){
+
+
+  
+
+
+
+    return 0;
+}
